00a92613c700aa71caa472b453088c15b6a502d7
joniizz <joniazeng@icloud.com>
2021-10-26 03:02:55 +1100

    design layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+//placed the Router and Switch components that were imported into the App div.
 const App = () =>{
-  return <div className='App'></div>;
-}
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+  </div>
+  );
+};
 
 export default App;
