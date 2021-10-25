d243d2e27c10d6d36b9c9fd23ad67e5cacd7c21d
joniizz <joniazeng@icloud.com>
2021-10-26 02:05:04 +1100

    backend user authentication api finish

--- manage.py
+++ manage.py
@@ -3,19 +3,10 @@
 import os
 import sys
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'app',
-]
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_signin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
